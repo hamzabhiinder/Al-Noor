6b3314ae6cebdd09282e14356495e84ae280f349
Aurooba Parker <aabathebest>
2024-08-24 18:37:22 +0500

    removed commented code

--- alnoor/lib/widgets/Input_Field.dart
+++ alnoor/lib/widgets/Input_Field.dart
@@ -1,52 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class CustomInputField extends StatelessWidget {
-//   final String hintText;
-//   final String iconAssetPath;
-//   final TextEditingController controller;
-//   final bool obscureText;
-//   final String? Function(String?)? validator;
-
-//   const CustomInputField({
-//     Key? key,
-//     required this.hintText,
-//     required this.iconAssetPath,
-//     required this.controller,
-//     this.obscureText = false,
-//     this.validator,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 8.0),
-//       child: TextFormField(
-//         controller: controller,
-//         obscureText: obscureText,
-//         decoration: InputDecoration(
-//           hintText: hintText,
-//           prefixIcon: Padding(
-//             padding: const EdgeInsets.all(12.0),
-//             child: Image.asset(iconAssetPath),
-//           ),
-//           filled: true,
-//           fillColor: Colors.white.withOpacity(0.8),
-//           border: OutlineInputBorder(
-//             borderRadius: BorderRadius.circular(10.0),
-//             borderSide: BorderSide.none,
-//           ),
-//         ),
-//         validator: validator,
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 
 class CustomInputField extends StatelessWidget {
@@ -68,7 +19,7 @@ class CustomInputField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    
+
     return Padding(
       padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.01),
       child: TextFormField(
--- alnoor/lib/widgets/Paginator.dart
+++ alnoor/lib/widgets/Paginator.dart
@@ -1,93 +1,7 @@
-// import 'package:flutter/material.dart';
-
-// List<Widget> buildPageIndicators(
-//     int totalPages, int currentPage, PageController _pageController) {
-//   List<Widget> indicators = [];
-
-//   if (totalPages <= 4) {
-//     for (int i = 0; i < totalPages; i++) {
-//       indicators.add(buildIndicator(i, _pageController, currentPage));
-//     }
-//   } else {
-//     indicators.add(buildIndicator(0, _pageController, currentPage));
-
-//     if (currentPage == 0) {
-//       indicators.add(buildIndicator(1, _pageController, currentPage));
-//     }
-
-//     if (currentPage > 1) {
-//       indicators.add(buildEllipsis());
-//     }
-
-//     if (currentPage > 0 && currentPage < totalPages - 1) {
-//       indicators.add(buildIndicator(currentPage, _pageController, currentPage));
-//     }
-
-//     if (currentPage < totalPages - 2) {
-//       indicators.add(buildEllipsis());
-//     }
-
-//     if (currentPage > totalPages - 2) {
-//       indicators
-//           .add(buildIndicator(totalPages - 2, _pageController, currentPage));
-//     }
-//     indicators
-//         .add(buildIndicator(totalPages - 1, _pageController, currentPage));
-//   }
-
-//   return indicators;
-// }
-
-// Widget buildIndicator(
-//     int pageIndex, PageController _pageController, int currentPage) {
-//   return GestureDetector(
-//     onTap: () {
-//       _pageController.jumpToPage(pageIndex);
-//     },
-//     child: Container(
-//       margin: EdgeInsets.symmetric(horizontal: 4.0),
-//       padding: EdgeInsets.all(6.0),
-//       decoration: BoxDecoration(
-//         shape: BoxShape.rectangle,
-//         border: Border.all(
-//           color: currentPage == pageIndex ? Colors.black : Colors.white,
-//           width: 1.0,
-//         ),
-//       ),
-//       child: Text(
-//         (pageIndex + 1).toString(),
-//         style: TextStyle(
-//           fontSize: 8,
-//           color: Colors.black,
-//         ),
-//       ),
-//     ),
-//   );
-// }
-
-// Widget buildEllipsis() {
-//   return Container(
-//     margin: EdgeInsets.symmetric(horizontal: 4.0),
-//     padding: EdgeInsets.all(6.0),
-//     child: Text(
-//       '...',
-//       style: TextStyle(
-//         fontSize: 8,
-//         color: Colors.black,
-//       ),
-//     ),
-//   );
-// }
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 
-List<Widget> buildPageIndicators(
-    int totalPages, int currentPage, PageController _pageController, BuildContext context) {
+List<Widget> buildPageIndicators(int totalPages, int currentPage,
+    PageController _pageController, BuildContext context) {
   List<Widget> indicators = [];
 
   if (totalPages <= 4) {
@@ -106,7 +20,8 @@ List<Widget> buildPageIndicators(
     }
 
     if (currentPage > 0 && currentPage < totalPages - 1) {
-      indicators.add(buildIndicator(currentPage, _pageController, currentPage, context));
+      indicators.add(
+          buildIndicator(currentPage, _pageController, currentPage, context));
     }
 
     if (currentPage < totalPages - 2) {
@@ -114,16 +29,18 @@ List<Widget> buildPageIndicators(
     }
 
     if (currentPage > totalPages - 2) {
-      indicators.add(buildIndicator(totalPages - 2, _pageController, currentPage, context));
+      indicators.add(buildIndicator(
+          totalPages - 2, _pageController, currentPage, context));
     }
-    indicators.add(buildIndicator(totalPages - 1, _pageController, currentPage, context));
+    indicators.add(
+        buildIndicator(totalPages - 1, _pageController, currentPage, context));
   }
 
   return indicators;
 }
 
-Widget buildIndicator(
-    int pageIndex, PageController _pageController, int currentPage, BuildContext context) {
+Widget buildIndicator(int pageIndex, PageController _pageController,
+    int currentPage, BuildContext context) {
   final screenSize = MediaQuery.of(context).size;
 
   return GestureDetector(
